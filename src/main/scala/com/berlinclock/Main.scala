b94c7340ec920a9b709977c0080963f558693844
Peter Belko <peter.belko@gmail.com>
2013-10-06 17:03:51 +0100

    Main class implementation - check input params - exception handling

--- src/main/scala/com/berlinclock/Main.scala
+++ src/main/scala/com/berlinclock/Main.scala
@@ -1,8 +1,26 @@
-package scala.com.berlinclock
+package com.berlinclock
+
+import java.text.ParseException
 
 /**
  * Main class of the app.
  */
 object Main extends App {
-  println("Hello World!")
+
+  if (args.length == 0) {
+    println("please specify the time (in the format 'hh:mm:ss') you want to display as the Berlin Clock.")
+  } else {
+    val time = args(0)
+
+    try {
+      println(new BerlinClock(time).clock)
+    } catch {
+      case e: ParseException => {
+        printf("'%s' is not a valid time. Please specify time in the format 'hh:mm:ss'.\n\n", time)
+      }
+      case _: Throwable => {
+        println("Something unexpected happened!")
+      }
+    }
+  }
 }
